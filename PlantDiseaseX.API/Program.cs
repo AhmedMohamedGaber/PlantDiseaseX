2c49cb15ad1d01518bd930a86a58c21ea69b10f6
AhmedMohamedGaber <85976531+AhmedMohamedGaber@users.noreply.github.com>
2024-02-25 23:44:01 +0200

    Add project files.

---
+++ PlantDiseaseX.API/Program.cs
@@ -0,0 +1,47 @@
+namespace PlantDiseaseX.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+
+            #endregion
+
+            var app = builder.Build();
+
+
+            #region Configure App
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers(); 
+
+
+
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}
